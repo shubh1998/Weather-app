5cfec21789f70c15731dc3253ea1bd151455b429
shubh1998 <shubhamgupta7440@gmail.com>
2021-10-24 00:44:14 +0530

    feat: added styled component file for ViewHistorical page

---
+++ src/pages/ViewHistorical/styles/ViewHistorical.StyledComponents.tsx
@@ -0,0 +1,16 @@
+import styled from '@emotion/styled'
+import { Card } from '@mui/material'
+
+export const DisplayFlex = styled.div({
+    display: 'flex'
+})
+
+export const Center = styled.div({
+  textAlign: 'center'
+})
+
+export const CardElement = styled(Card)({
+  "&&":{
+    boxShadow: '0px 1px 3px 0px rgb(0 0 0 / 12%), 0px 1px 3px 0px rgb(0 0 0 / 12%), 0px 1px 3px 0px rgb(0 0 0 / 12%), 0px 1px 3px 0px rgb(0 0 0 / 12%)'
+  }
+})
